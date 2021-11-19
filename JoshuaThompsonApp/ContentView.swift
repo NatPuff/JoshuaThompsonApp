8b7dbca898a16707cdd318a56fd7ea5437d9cf9a
NatPuff <cesarp102104@gmail.com>
2021-11-19 08:23:44 -0600

    Initial Commit

---
+++ JoshuaThompsonApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JoshuaThompsonApp
+//
+//  Created by Cesar Pacheco on 11/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JoshuaThompsonApp/JoshuaThompsonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshuaThompsonAppApp.swift
+//  JoshuaThompsonApp
+//
+//  Created by Cesar Pacheco on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct JoshuaThompsonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
